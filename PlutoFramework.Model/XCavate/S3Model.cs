00192212bcc3da6a055ea9ddae8f3030e4db34d8
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2025-01-18 10:47:18 +0100

    S3Model temp

---
+++ PlutoFramework.Model/XCavate/S3Model.cs
@@ -0,0 +1,69 @@
+﻿using Amazon.S3.Model;
+using Amazon.S3;
+using Amazon;
+
+namespace PlutoFramework.Model.XCavate
+{
+    public class S3Model
+    {
+        public static void TEMPMain()
+        {
+            const string bucketName = "amzn-s3-demo-bucket";
+            const string objectKey = "sample.txt";
+
+            // Specify how long the presigned URL lasts, in hours
+            const double timeoutDuration = 12;
+
+            // Specify the AWS Region of your Amazon S3 bucket. If it is
+            // different from the Region defined for the default user,
+            // pass the Region to the constructor for the client. For
+            // example: new AmazonS3Client(RegionEndpoint.USEast1);
+
+            // If using the Region us-east-1, and server-side encryption with AWS KMS, you must specify Signature Version 4.
+            // Region us-east-1 defaults to Signature Version 2 unless explicitly set to Version 4 as shown below.
+            // For more details, see https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingAWSSDK.html#specify-signature-version
+            // and https://docs.aws.amazon.com/sdkfornet/v3/apidocs/items/Amazon/TAWSConfigsS3.html
+            AWSConfigsS3.UseSignatureVersion4 = true;
+            IAmazonS3 s3Client = new AmazonS3Client(RegionEndpoint.EUWest1);
+
+            string urlString = GeneratePresignedURL(s3Client, bucketName, objectKey, timeoutDuration);
+            Console.WriteLine($"The generated URL is: {urlString}.");
+        }
+
+        /// <summary>
+        /// Source: https://docs.aws.amazon.com/code-library/latest/ug/s3_example_s3_Scenario_PresignedUrl_section.html
+        /// Generate a presigned URL that can be used to access the file named
+        /// in the objectKey parameter for the amount of time specified in the
+        /// duration parameter.
+        /// </summary>
+        /// <param name="client">An initialized S3 client object used to call
+        /// the GetPresignedUrl method.</param>
+        /// <param name="bucketName">The name of the S3 bucket containing the
+        /// object for which to create the presigned URL.</param>
+        /// <param name="objectKey">The name of the object to access with the
+        /// presigned URL.</param>
+        /// <param name="duration">The length of time for which the presigned
+        /// URL will be valid.</param>
+        /// <returns>A string representing the generated presigned URL.</returns>
+        public static string GeneratePresignedURL(IAmazonS3 client, string bucketName, string objectKey, double duration)
+        {
+            string urlString = string.Empty;
+            try
+            {
+                var request = new GetPreSignedUrlRequest()
+                {
+                    BucketName = bucketName,
+                    Key = objectKey,
+                    Expires = DateTime.UtcNow.AddHours(duration),
+                };
+                urlString = client.GetPreSignedURL(request);
+            }
+            catch (AmazonS3Exception ex)
+            {
+                Console.WriteLine($"Error:'{ex.Message}'");
+            }
+
+            return urlString;
+        }
+    }
+}
