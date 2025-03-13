7324be58004b5df201c399ebe41b13e638213f2e
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2025-03-13 12:23:49 +0100

    Update address copy

--- PlutoFramework/Components/AddressView/AddressQrCodeViewModel.cs
+++ PlutoFramework/Components/AddressView/AddressQrCodeViewModel.cs
@@ -8,13 +8,11 @@ namespace PlutoFramework.Components.AddressView
         [ObservableProperty]
         private string qrAddress;
 
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(TwoLineAddress))]
         private string address;
 
-        public string Address
-        {
-            get => address;
-            set => SetProperty(ref address, value.Substring(0, value.Length / 2) + "\n" + value.Substring(value.Length / 2));
-        }
+        public string TwoLineAddress => Address.Substring(0, Address.Length / 2) + "\n" + Address.Substring(Address.Length / 2);
 
         [ObservableProperty]
         private bool isVisible = false;
