209949242245a005378b33bf8f9ce8a7da0b9d32
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2025-03-13 14:40:17 +0100

    Network select performance improvements

--- PlutoFramework/Components/NetworkSelect/NetworkSelectInfo.cs
+++ PlutoFramework/Components/NetworkSelect/NetworkSelectInfo.cs
@@ -1,24 +1,43 @@
 ﻿using PlutoFramework.Constants;
 using System;
+using System.ComponentModel;
 namespace PlutoFramework.Components.NetworkSelect
 {
-    public class NetworkSelectInfo
+    public class NetworkSelectInfo : INotifyPropertyChanged
     {
         public EndpointEnum EndpointKey { get; set; }
         public bool ShowName { get; set; }
         public string Name { get; set; }
         public string Icon { get; set; }
         public string DarkIcon { get; set; }
-        public bool IsSelected { get; set; }
         public EndpointConnectionStatus EndpointConnectionStatus { get; set; }
-    }
 
-    public enum EndpointConnectionStatus
-    {
-        None, // This is the ViewModel bug in .net MAUI, it has to be here
-        Loading,
-        Connected,
-        Failed,
+
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get => isSelected;
+            set
+            {
+                if (isSelected != value)
+                {
+                    isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+}
+
+public enum EndpointConnectionStatus
+{
+    None, // This is the ViewModel bug in .net MAUI, it has to be here
+    Loading,
+    Connected,
+    Failed,
 }
 
