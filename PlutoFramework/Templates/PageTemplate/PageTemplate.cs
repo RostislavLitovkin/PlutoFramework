10fa49f6b9be09f91d3a825f294ce0e2ca704816
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2025-07-08 08:09:18 +0200

    Update template parameters

--- PlutoFramework/Templates/PageTemplate/PageTemplate.cs
+++ PlutoFramework/Templates/PageTemplate/PageTemplate.cs
@@ -1,4 +1,5 @@
-﻿using MauiView = Microsoft.Maui.Controls.View;
+﻿using CommunityToolkit.Mvvm.Input;
+using MauiView = Microsoft.Maui.Controls.View;
 
 namespace PlutoFramework.Templates.PageTemplate
 {
@@ -19,6 +20,54 @@ namespace PlutoFramework.Templates.PageTemplate
         {
             get => (MauiView)GetValue(PopupContentProperty);
             set => SetValue(PopupContentProperty, value);
+        }
+
+        public static readonly BindableProperty NavigationBarExtra1TextProperty =
+           BindableProperty.Create(nameof(NavigationBarExtra1Text), typeof(string), typeof(PageTemplate));
+        public string NavigationBarExtra1Text
+        {
+            get => (string)GetValue(NavigationBarExtra1TextProperty);
+            set => SetValue(NavigationBarExtra1TextProperty, value);
+        }
+
+        public static readonly BindableProperty NavigationBarExtra1CommandProperty =
+           BindableProperty.Create(nameof(NavigationBarExtra1Command), typeof(IAsyncRelayCommand), typeof(PageTemplate));
+        public string NavigationBarExtra1Command
+        {
+            get => (string)GetValue(NavigationBarExtra1CommandProperty);
+            set => SetValue(NavigationBarExtra1CommandProperty, value);
+        }
+
+        public static readonly BindableProperty NavigationBarExtra1ImageProperty =
+           BindableProperty.Create(nameof(NavigationBarExtra1Image), typeof(string), typeof(PageTemplate));
+        public string NavigationBarExtra1Image
+        {
+            get => (string)GetValue(NavigationBarExtra1ImageProperty);
+            set => SetValue(NavigationBarExtra1ImageProperty, value);
+        }
+
+        public static readonly BindableProperty NavigationBarExtra2TextProperty =
+           BindableProperty.Create(nameof(NavigationBarExtra2Text), typeof(string), typeof(PageTemplate));
+        public string NavigationBarExtra2Text
+        {
+            get => (string)GetValue(NavigationBarExtra2TextProperty);
+            set => SetValue(NavigationBarExtra2TextProperty, value);
+        }
+
+        public static readonly BindableProperty NavigationBarExtra2CommandProperty =
+           BindableProperty.Create(nameof(NavigationBarExtra2Command), typeof(IAsyncRelayCommand), typeof(PageTemplate));
+        public string NavigationBarExtra2Command
+        {
+            get => (string)GetValue(NavigationBarExtra2CommandProperty);
+            set => SetValue(NavigationBarExtra2CommandProperty, value);
+        }
+
+        public static readonly BindableProperty NavigationBarExtra2ImageProperty =
+           BindableProperty.Create(nameof(NavigationBarExtra2Image), typeof(string), typeof(PageTemplate));
+        public string NavigationBarExtra2Image
+        {
+            get => (string)GetValue(NavigationBarExtra2ImageProperty);
+            set => SetValue(NavigationBarExtra2ImageProperty, value);
         }
 
         public PageTemplate()
--- PlutoFramework/Templates/TopNavigationBarTemplate/TopNavigationBarTemplate.cs
+++ PlutoFramework/Templates/TopNavigationBarTemplate/TopNavigationBarTemplate.cs
@@ -1,9 +1,66 @@
-﻿using Microsoft.Maui.Layouts;
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Layouts;
 
 namespace PlutoFramework.Templates.TopNavigationBarTemplate
 {
     public class TopNavigationBarTemplate : ContentView
     {
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create(nameof(Title), typeof(string), typeof(TopNavigationBarTemplate));
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly BindableProperty Extra1TextProperty =
+           BindableProperty.Create(nameof(Extra1Text), typeof(string), typeof(TopNavigationBarTemplate));
+        public string Extra1Text
+        {
+            get => (string)GetValue(Extra1TextProperty);
+            set => SetValue(Extra1TextProperty, value);
+        }
+
+        public static readonly BindableProperty Extra1ImageProperty =
+           BindableProperty.Create(nameof(Extra1Image), typeof(string), typeof(TopNavigationBarTemplate));
+        public string Extra1Image
+        {
+            get => (string)GetValue(Extra1ImageProperty);
+            set => SetValue(Extra1ImageProperty, value);
+        }
+
+        public static readonly BindableProperty Extra1CommandProperty =
+           BindableProperty.Create(nameof(Extra1Command), typeof(IAsyncRelayCommand), typeof(TopNavigationBarTemplate));
+        public string Extra1Command
+        {
+            get => (string)GetValue(Extra1CommandProperty);
+            set => SetValue(Extra1CommandProperty, value);
+        }
+
+        public static readonly BindableProperty Extra2TextProperty =
+           BindableProperty.Create(nameof(Extra2Text), typeof(string), typeof(TopNavigationBarTemplate));
+        public string Extra2Text
+        {
+            get => (string)GetValue(Extra2TextProperty);
+            set => SetValue(Extra2TextProperty, value);
+        }
+
+        public static readonly BindableProperty Extra2ImageProperty =
+           BindableProperty.Create(nameof(Extra2Image), typeof(string), typeof(TopNavigationBarTemplate));
+        public string Extra2Image
+        {
+            get => (string)GetValue(Extra2ImageProperty);
+            set => SetValue(Extra2ImageProperty, value);
+        }
+
+        public static readonly BindableProperty Extra2CommandProperty =
+           BindableProperty.Create(nameof(Extra2Command), typeof(IAsyncRelayCommand), typeof(TopNavigationBarTemplate));
+        public string Extra2Command
+        {
+            get => (string)GetValue(Extra2CommandProperty);
+            set => SetValue(Extra2CommandProperty, value);
+        }
+
         public TopNavigationBarTemplate()
         {
             BindingContext = new TopNavigationBarViewModel();
