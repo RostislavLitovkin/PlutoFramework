9459f2224a77faa4b7cff905eee11cbac5993cd7
valentynhol <hol.valentyn@gmail.com>
2025-08-14 01:34:03 +0200

    Added outgoing message view

--- PlutoFramework/Components/Messaging/IncomingMessageView.xaml.cs
+++ PlutoFramework/Components/Messaging/IncomingMessageView.xaml.cs
@@ -1,12 +1,48 @@
-using PlutoFramework.Templates.PageTemplate;
-
 namespace PlutoFramework.Components.Messaging
 {
     public partial class IncomingMessageView: ContentView
     {
+        public static readonly BindableProperty MsgColorProperty = BindableProperty.Create(nameof(MsgColor), 
+            typeof(Color), typeof(IncomingMessageView), Application.Current.Resources["X-LEAFGREEN"] as Color);
+
+        public Color MsgColor
+        {
+            get => (Color)GetValue(MsgColorProperty);
+            set => SetValue(MsgColorProperty, value);
+        }
+        
+        public static readonly BindableProperty SenderProperty = BindableProperty.Create(nameof(Sender), typeof(string),
+            typeof(IncomingMessageView));
+
+        public string Sender
+        {
+            get => (string)GetValue(SenderProperty);
+            set => SetValue(SenderProperty, value);
+        }
+        
+        public static readonly BindableProperty MessageProperty = BindableProperty.Create(nameof(Message), 
+            typeof(string), typeof(IncomingMessageView));
+
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+        
+        public static readonly BindableProperty TimestampProperty = BindableProperty.Create(nameof(Timestamp), 
+            typeof(string), typeof(IncomingMessageView));
+
+        public string Timestamp
+        {
+            get => (string)GetValue(TimestampProperty);
+            set => SetValue(TimestampProperty, value);
+        }
+        
         public IncomingMessageView()
         {
             InitializeComponent();
+            
+            BindingContext = this;
         }
     }
 }
---
+++ PlutoFramework/Components/Messaging/OutgoingMessageView.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace PlutoFramework.Components.Messaging;
+
+public partial class OutgoingMessageView : ContentView
+{
+    public static readonly BindableProperty MessageProperty = BindableProperty.Create(nameof(Message), 
+        typeof(string), typeof(IncomingMessageView));
+
+    public string Message
+    {
+        get => (string)GetValue(MessageProperty);
+        set => SetValue(MessageProperty, value);
+    }
+        
+    public static readonly BindableProperty TimestampProperty = BindableProperty.Create(nameof(Timestamp), 
+        typeof(string), typeof(IncomingMessageView));
+
+    public string Timestamp
+    {
+        get => (string)GetValue(TimestampProperty);
+        set => SetValue(TimestampProperty, value);
+    }
+    
+    public OutgoingMessageView()
+    {
+        InitializeComponent();
+        
+        BindingContext = this;
+    }
+}
