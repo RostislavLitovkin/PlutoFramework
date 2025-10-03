572b765c2a07d63bcbcb0b666c27db6e2e89266c
valentynhol <hol.valentyn@gmail.com>
2025-10-03 17:26:03 +0200

    Add ChatsOverviewPage

---
+++ PlutoFramework/Components/Messaging/ChatItem.cs
@@ -0,0 +1,10 @@
+﻿namespace PlutoFramework.Components.Messaging;
+
+public record ChatItem
+{
+    public required string Title { get; set; }
+    public required string State { get; set; }
+    public required string Time { get; set; }
+    public required string IsApproved { get; set; }
+    public required Color IsApprovedColor { get; set; }
+}
---
+++ PlutoFramework/Components/Messaging/ChatsOverviewPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using PlutoFramework.Templates.PageTemplate;
+
+namespace PlutoFramework.Components.Messaging;
+
+public partial class ChatsOverviewPage : PageTemplate
+{
+    public ObservableCollection<ChatItem> Chats { get; } = new();
+    public ChatsOverviewPage()
+    {
+        InitializeComponent();
+    }
+}
